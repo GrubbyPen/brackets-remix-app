5f1ec0c9247f4002b92ff875fb83cc448c1d8b1b
Kelvin Lim <kelvin@x15.com.au>
2022-12-23 18:08:11 +1000

    feat: Add blue remix starter and tournaments

---
+++ app/models/tournament.server.ts
@@ -0,0 +1,64 @@
+import type { User, Tournament } from "@prisma/client";
+
+import { prisma } from "~/db.server";
+
+export type { Tournament } from "@prisma/client";
+
+export function getTournament({
+  id,
+  userId,
+}: Pick<Tournament, "id"> & {
+  userId: User["id"];
+}) {
+  return prisma.tournament.findFirst({
+    select: { id: true, description: true, title: true },
+    where: {
+      AND: [
+        {
+          id,
+        },
+        {
+          users: { some: {} },
+        },
+      ],
+    },
+  });
+}
+
+export function getTournamentListItems({ userId }: { userId: User["id"] }) {
+  return prisma.user
+    .findUniqueOrThrow({
+      where: { id: userId },
+    })
+    .tournaments();
+}
+
+export function createTournament({
+  description,
+  title,
+  userId,
+}: Pick<Tournament, "description" | "title"> & {
+  userId: User["id"];
+}) {
+  return prisma.tournament.create({
+    data: {
+      title,
+      description,
+      users: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+}
+
+export function deleteTournament({
+  id,
+  userId,
+}: Pick<Tournament, "id"> & { userId: User["id"] }) {
+  // todo add authorisation guard
+  return prisma.tournament.deleteMany({
+    where: { id },
+  });
+}
